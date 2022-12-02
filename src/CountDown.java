4223ade1c61f660468dac2c53baca0b67124f126
Henry <henryw48@nycstudents.net>
2022-12-02 12:17:47 -0500

    added code

--- src/CountDown.java
+++ src/CountDown.java
@@ -6,6 +6,8 @@ public class CountDown {
 
     private int christmas = 359;
 
+
+
     final int year = 2022;
 
     /* These are the months and how many days are in each month */
@@ -27,7 +29,7 @@ public class CountDown {
         /* gets the date of the user choice */
         month = m;
         day = d;
-        System.out.println("Today is " + m + "/" + d + "/" + year);
+        System.out.println("Today is " + m + "/" + d + "/" + year + ".");
     }
 
     public String CountDown() {
@@ -43,36 +45,47 @@ public class CountDown {
             System.out.println("Date is January" + day);
         }
         else if (month == 2) {
+            int total = January + February;
             System.out.println("Date is February");
         }
         else if (month == 3) {
+            int total = January + February + March;
             System.out.println("Date is March");
         }
         else if (month == 4) {
+            int total = January + February + March + April;
             System.out.println("Date is April");
         }
         else if (month == 5) {
+            int total = January + February + March + April + May;
             System.out.println("Date is May");
         }
         else if (month == 6) {
+            int total = January + February + March + April + May + June;
             System.out.println("Date is June");
         }
         else if (month == 7) {
+            int total = January + February + March + April + May + June + July;
             System.out.println("Date is July");
         }
         else if (month == 8) {
+            int total = January + February + March + April + May + June + July + August;
             System.out.println("Date is August");
         }
         else if (month == 9) {
+            int total = January + February + March + April + May + June + July + August + September;
             System.out.println("Date is September");
         }
         else if (month == 10) {
+            int total = January + February + March + April + May + June + July + August + September + October;
             System.out.println("Date is October");
         }
         else if (month == 11) {
+            int total = January + February + March + April + May + June + July + August + September + October + November;
             System.out.println("Date is November");
         }
         else {
+            int total = January + February + March + April + May + June + July + August + September + October + November + December;
             System.out.println("Date is December");
         }
         return 0;
